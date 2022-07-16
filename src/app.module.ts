2032c148f5a251f3ab912d7538e41a587b01d7d7
Alexey Pirozerskiy <pirozerskiy.a@wb.ru>
2022-07-16 20:01:20 +0300

    init:start task

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumController } from './album/album.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AlbumController],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
